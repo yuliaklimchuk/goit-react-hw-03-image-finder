bc5409b467850df1a2c6ee5c239a4ee43ca303d3
Yulia Klimchuk <yulia.melnichenko.995@gmail.com>
2022-05-15 22:16:49 +0300

    Edits

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,71 @@
 import React, { Component } from "react";
-import { ToastContainer } from 'react-toastify';
+import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-
 import style from './app.module.css';
 import { Searchbar } from "./Searchbar";
 import { ImageGallery } from "./ImageGallery";
+import { Loader } from "./Loader";
+import { Button } from "./Button";
+
+const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '25800481-71cffbd2e779364a85bf72062';
 
 export class App extends Component { 
   state = {
     imageName: '',
+    images: [],
+    loading: false,
+    error: null
   }
+  componentDidUpdate(prevProps, prevStates) { 
+    const nextName = this.state.imageName;
+      if (prevStates.imageName !== nextName) { 
+        this.setState({
+          images: [],
+          loading: true
+        });
+        setTimeout(() => {
+          fetch(`${BASE_URL}?q=${nextName}&page=1&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`)
+            .then(response => {
+              if (response.ok) {
+                return response.json();
+              }
+              return Promise.reject(
+                new Error('Images not found. Please enter a valid value'),
+              );
+            })
+            .then(images => {
+              if (images.hits.length === 0) {
+                return Promise.reject(
+                new Error('Images not found. Please enter a valid value'),
+              );
+              }
+              this.setState({ images: images.hits })
+            })
+            .catch((error) => this.fetchError(error))
+            .finally(() => this.setState({ loading: false }))
+        },1000);
+        }
+    }
   handleFormSubmit = (imageName) => { 
     this.setState({ imageName: imageName });
   }
+  fetchError = (error) => { 
+    this.setState({
+      images: [],
+      error: error
+    })
+    toast.error(error.message);
+  }
   render() { 
+    const {images, loading } = this.state;
     return (
       <div className={ style.app}>
         <ToastContainer />
         <Searchbar onSubmit={this.handleFormSubmit} />
-        <ImageGallery imageName={ this.state.imageName}/>
+        { loading && <Loader />}
+        {(images.length !== 0) && <ImageGallery images={images} />}
+        {(images.length !== 0) && <Button />}
       </div>
     )
   }
---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,7 @@
+import style from './button.module.css';
+
+export const Button = () => { 
+    return (
+        <button type="button" className={ style.button}>Load more</button>
+    )
+}
